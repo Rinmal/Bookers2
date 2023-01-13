1164ea6cdda22108fab65783f5e2434e4e54847c
EC2 Default User <ec2-user@ip-172-31-47-66.ap-northeast-1.compute.internal>
2023-01-13 12:57:14 +0000

    [Add] ファイルの記述少し進めた

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit]
   resources :books, only: [:index, :show, :edit]
   get 'about' => 'homes#about'
+  patch 'users/:id' => 'users#update', as: 'update_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

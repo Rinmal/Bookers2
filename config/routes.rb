08926a2bd6a121911f03beaf82d2805b69daf37f
Rinmal <121992801+Rinmal@users.noreply.github.com>
2023-01-14 13:03:46 +0000

    [Add] UsersInfoとNewBookを部分テンプレートにした/createできないエラーあり

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :edit, :create]
   get 'about' => 'homes#about'
-  patch 'users/:id' => 'users#update', as: 'update_user'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

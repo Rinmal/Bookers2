23e56d719e28c76408c10a0bb2c5b76f011c4259
Rinmal <121992801+Rinmal@users.noreply.github.com>
2023-02-05 04:48:11 +0000

    [Add]メール送信機能の追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,9 +70,23 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      port:                 587,
+      address:              'smtp.gmail.com',
+      domain:               'gmail.com',
+      user_name:            'rin.66mal@gmail.com',#Gmailのアドレス
+      password:             'wryvaiyntwbsnsad',#後述！大苦戦しました！
+      authentication:       'login',
+      enable_starttls_auto: true
+    }
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts.clear
   config.active_job.queue_adapter = :inline
+
+  # config.action_mailer.default_url_options = {host: 'myapp.c9users.io/', port: $PORT, protocol: 'https' }
+  # config.action_mailer.delivery_method = :letter_opener_web
 end
